622c577003bb29b6eb7c3bafa8f6a1a42e35c479
pabloparejo <parejo.p@gmail.com>
2015-05-08 18:55:14 +0200

    added CoreDataTableVC

--- Everpobre/PARNotesViewController.h
+++ Everpobre/PARNotesViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PARNotesViewController.h
 //  Everpobre
 //
 //  Created by Pablo Parejo Camacho on 8/5/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PARNotesViewController : UITableViewController
 
 @end
-
