2a84f744cd6fe204d8948f5d10bd014fc1f6aaea
pabloparejo <parejo.p@gmail.com>
2015-05-08 17:45:07 +0200

    added Note

---
+++ Everpobre/Note.h
@@ -0,0 +1,20 @@
+//
+//  Note.h
+//  Everpobre
+//
+//  Created by Pablo Parejo Camacho on 8/5/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Note : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSData * photo;
+@property (nonatomic, retain) NSString * text;
+@property (nonatomic, retain) NSDate * creationDate;
+
+@end
---
+++ Everpobre/Note.m
@@ -0,0 +1,19 @@
+//
+//  Note.m
+//  Everpobre
+//
+//  Created by Pablo Parejo Camacho on 8/5/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "Note.h"
+
+
+@implementation Note
+
+@dynamic title;
+@dynamic photo;
+@dynamic text;
+@dynamic creationDate;
+
+@end
